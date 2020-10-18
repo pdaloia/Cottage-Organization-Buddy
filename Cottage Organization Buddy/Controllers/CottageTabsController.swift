c02d0a2df29a70b9162711dc27352c35a6156188
Philip D'Aloia <philip.daloia@gmail.com>
2020-10-18 19:02:33 -0400

    Programmatically configured the CottageTabs controller to load the controllers and views of the cottage sections (groceries, cars, beds, and drinks)

--- Cottage Organization Buddy/Controllers/CottageTabsController.swift
+++ Cottage Organization Buddy/Controllers/CottageTabsController.swift
@@ -12,7 +12,27 @@ class CottageTabsController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        loadViewControllers()
+    }
+    
+    func loadViewControllers() {
+        //initialize the storyboards with their respective controllers
+        let groceriesViewController = UIStoryboard(name: "Groceries", bundle:nil).instantiateViewController(identifier: "GroceriesView") as GroceriesController
+        let carsViewController = UIStoryboard(name: "Cars", bundle:nil).instantiateViewController(identifier: "CarsView") as CarsController
+        let bedsViewController = UIStoryboard(name: "Beds", bundle: nil).instantiateViewController(identifier: "BedsView") as BedsController
+        let drinksViewContoller = UIStoryboard(name: "Drinks", bundle: nil).instantiateViewController(identifier: "DrinksView") as DrinksController
+        
+        //create the tab bar items for each view controller
+        groceriesViewController.tabBarItem = UITabBarItem(tabBarSystemItem: .more, tag: 0)
+        carsViewController.tabBarItem = UITabBarItem(tabBarSystemItem: .more, tag: 1)
+        bedsViewController.tabBarItem = UITabBarItem(tabBarSystemItem: .more, tag: 2)
+        drinksViewContoller.tabBarItem = UITabBarItem(tabBarSystemItem: .more, tag: 3)
+        
+        //create a list of all the controllers that will be loaded into the tab bar
+        let tabBarList = [groceriesViewController, carsViewController, bedsViewController, drinksViewContoller]
+        
+        //load the tab bar
+        viewControllers = tabBarList
     }
 
 }
