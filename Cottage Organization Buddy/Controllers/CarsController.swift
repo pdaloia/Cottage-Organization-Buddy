c6de5e9640867a11c73183c3384324eb2755d52c
Philip D'Aloia <philip.daloia@gmail.com>
2020-10-22 22:45:34 -0400

    Created attendees view,  model, and controller

--- Cottage Organization Buddy/Controllers/CarsController.swift
+++ Cottage Organization Buddy/Controllers/CarsController.swift
@@ -10,9 +10,57 @@ import UIKit
 class CarsController: UIViewController, TabBarItemControllerProtocol {
     
     var cottageModel: CottageTrip?
-
+    
+    @IBOutlet weak var carsCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
+        
+        carsCollectionView.dataSource = self
+        carsCollectionView.delegate = self
+        
     }
+    
+}
 
+extension CarsController: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return cottageModel!.carsList.count
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CarCell", for: indexPath)
+        
+        let carImageView = UIImageView(image: UIImage(systemName: "car"))
+        carImageView.frame = cell.contentView.bounds
+        carImageView.center = CGPoint(x: cell.bounds.width/2, y: cell.bounds.height/2)
+        carImageView.contentMode = .scaleAspectFit
+        cell.addSubview(carImageView)
+        
+        return cell
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let columns: CGFloat = 2
+        
+        let collectionViewWidth = collectionView.bounds.width
+        let flowLayout = collectionViewLayout as! UICollectionViewFlowLayout
+        let spaceBetweenCells = flowLayout.minimumInteritemSpacing * (columns - 1)
+        
+        let adjustedWidth = collectionViewWidth - spaceBetweenCells
+        
+        let width: CGFloat = floor(adjustedWidth / columns)
+        let height: CGFloat = width
+        
+        return CGSize(width: width, height: height)
+        
+    }
+    
 }
