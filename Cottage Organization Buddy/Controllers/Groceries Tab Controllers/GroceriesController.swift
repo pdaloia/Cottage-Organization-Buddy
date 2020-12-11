92534d2125e38a5d691404b7adf5b8776fa94fc0
Philip D'Aloia <philip.daloia@gmail.com>
2020-12-10 20:27:29 -0500

    Recreated the grocery controller and the collection view for it. Used the tile look, while separating the collective grocery list from the personal grocery lists.

--- Cottage Organization Buddy/Controllers/Groceries Tab Controllers/GroceriesController.swift
+++ Cottage Organization Buddy/Controllers/Groceries Tab Controllers/GroceriesController.swift
@@ -10,92 +10,42 @@ import UIKit
 class GroceriesController: UIViewController, TabBarItemControllerProtocol {
     
     var cottageModel: CottageTrip?
-    var groceriesTableView: UITableView?
-    var navBar: UINavigationBar?
+    
+    var groceriesCollectionView: GroceriesCollectionView?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         title = "Groceries"
         
-        //set up groceries table view
-        groceriesTableView = UITableView()
-        groceriesTableView?.dataSource = self
-        groceriesTableView?.delegate = self
-        setGroceriesTableViewConstraints()
+        self.view.backgroundColor = .systemBackground
+        
+        initializeCollectionView()
+    }
+    
+    func initializeCollectionView() {
+        
+        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 10, left: 3, bottom: 10, right: 3)
+        layout.itemSize = CGSize(width: 60, height: 60)
+        
+        groceriesCollectionView = GroceriesCollectionView(frame: self.view.frame, collectionViewLayout: layout)
+        groceriesCollectionView?.cottageModel = cottageModel
+        
+        groceriesCollectionView?.register(GroceriesCollectionViewCell.self, forCellWithReuseIdentifier: "GroceriesCell")
+        
+        groceriesCollectionView?.delegate = groceriesCollectionView
+        groceriesCollectionView?.dataSource = groceriesCollectionView
+        
+        groceriesCollectionView?.backgroundColor = .clear
+        view.addSubview(groceriesCollectionView!)
+        
+        groceriesCollectionView?.translatesAutoresizingMaskIntoConstraints = false
+        groceriesCollectionView?.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor).isActive = true
+        groceriesCollectionView?.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        groceriesCollectionView?.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        groceriesCollectionView?.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+        
     }
 
-}
-
-extension GroceriesController: UITableViewDataSource, UITableViewDelegate {
-    
-    func setGroceriesTableViewConstraints() {
-        //add table to the view and set its constraints
-        self.view.addSubview(groceriesTableView!)
-        groceriesTableView?.translatesAutoresizingMaskIntoConstraints = false
-        groceriesTableView?.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
-        groceriesTableView?.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
-        groceriesTableView?.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
-        groceriesTableView?.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
-        
-    }
-    
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 2
-    }
-    
-    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        switch section {
-        case 1:
-            return "Personal Groceries Lists"
-        default:
-            return nil
-        }
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        switch section {
-        case 0:
-            return 1
-        case 1:
-            return cottageModel!.groceryList.groceriesPerPerson.count
-        default:
-            return 0
-        }
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let currentCell = GroceriesTableViewCell()
-        
-        switch indexPath.section {
-        case 0:
-            currentCell.textLabel?.text = "All Grocery Items"
-        case 1:
-            currentCell.textLabel?.text = cottageModel!.groceryList.groceriesPerPerson[indexPath.row].person.name + "'s grocery list"
-        default:
-            currentCell.textLabel?.text = "Error"
-        }
-        
-        return currentCell
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
-        let groceryListController = GroceryListController()
-        
-        switch(indexPath.section) {
-        case 0:
-            groceryListController.groceryListToDisplay = self.cottageModel?.groceryList.allItems
-            groceryListController.groceryListTitle = "All Items"
-        case 1:
-            groceryListController.groceryListToDisplay = self.cottageModel?.groceryList.groceriesPerPerson[indexPath.item].groceries
-            groceryListController.groceryListTitle = "\(self.cottageModel?.groceryList.groceriesPerPerson[indexPath.item].person.name ?? "Error")'s List"
-        default:
-            groceryListController.groceryListToDisplay = []
-        }
-        
-        self.navigationController?.pushViewController(groceryListController, animated: true)
-        
-    }
-    
 }
---
+++ Cottage Organization Buddy/Views/Groceries Tab Views/GroceriesCollectionViewCell.swift
@@ -0,0 +1,46 @@
+//
+//  GroceriesTableViewCell.swift
+//  Cottage Organization Buddy
+//
+//  Created by Philip D'Aloia on 2020-10-29.
+//
+
+import UIKit
+
+class GroceriesCollectionViewCell: UICollectionViewCell {
+    
+    var cellTitle: String?
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    func setupGroceryCell() {
+        
+        //set up the cells label
+        let cellTitleLabel = UILabel()
+        cellTitleLabel.text = cellTitle!
+        cellTitleLabel.textAlignment = .center
+        
+        self.contentView.addSubview(cellTitleLabel)
+        cellTitleLabel.translatesAutoresizingMaskIntoConstraints = false
+        cellTitleLabel.topAnchor.constraint(equalTo: self.contentView.topAnchor).isActive = true
+        cellTitleLabel.heightAnchor.constraint(equalTo: self.contentView.heightAnchor, multiplier: 0.5).isActive = true
+        cellTitleLabel.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor).isActive = true
+        cellTitleLabel.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor).isActive = true
+        
+        //set up the cells image
+        let cellImage = UIImage(systemName: "person")
+        let cellImageView = UIImageView(image: cellImage?.withRenderingMode(.alwaysOriginal))
+        cellImageView.contentMode = .scaleAspectFit
+        
+        self.contentView.addSubview(cellImageView)
+        cellImageView.translatesAutoresizingMaskIntoConstraints = false
+        cellImageView.topAnchor.constraint(equalTo: cellTitleLabel.bottomAnchor).isActive = true
+        cellImageView.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor).isActive = true
+        cellImageView.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor).isActive = true
+        cellImageView.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor).isActive = true
+        
+    }
+
+}
