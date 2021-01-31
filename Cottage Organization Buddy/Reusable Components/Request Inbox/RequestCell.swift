36ed70039c23b35a318d942b62e79056eb48dc1c
Philip D'Aloia <philip.daloia@gmail.com>
2021-01-31 17:50:34 -0500

    Folder restructure. Creating reusable component for all request inboxes. Going to use the request protocol so that any request model that implements this protocol can be used with this inbox component.

---
+++ Cottage Organization Buddy/Reusable Components/Request Inbox/RequestCell.swift
@@ -0,0 +1,12 @@
+//
+//  RequestCell.swift
+//  Cottage Organization Buddy
+//
+//  Created by Philip D'Aloia on 2021-01-31.
+//
+
+import UIKit
+
+class RequestCell: UICollectionViewCell {
+    
+}
