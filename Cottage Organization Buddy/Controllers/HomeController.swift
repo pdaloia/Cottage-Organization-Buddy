e76c5d52194c86b1007ef0000f5ed76ff7600ff1
Philip D'Aloia <philip.daloia@gmail.com>
2021-03-07 20:15:03 -0500

    App now loads the proper view when the user has previously signed in.

--- Cottage Organization Buddy/Controllers/HomeController.swift
+++ Cottage Organization Buddy/Controllers/HomeController.swift
@@ -33,6 +33,22 @@ class HomeController: UIViewController {
         googleSignInbutton.translatesAutoresizingMaskIntoConstraints = false
         googleSignInbutton.topAnchor.constraint(equalTo: self.homeLabel!.bottomAnchor).isActive = true
         googleSignInbutton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+                
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        super.viewDidAppear(false)
+        
+        //check if user is already signed in here
+        if GIDSignIn.sharedInstance().hasPreviousSignIn() {
+            
+            GIDSignIn.sharedInstance()?.restorePreviousSignIn()
+            
+        }
+        else {
+            print("not signed in")
+        }
         
     }
 
@@ -44,8 +60,22 @@ extension HomeController: GIDSignInDelegate{
     func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
         
         if let error = error {
-            // ...
+            print("\(error.localizedDescription)")
             return
+        } else {
+            let userId = user.userID                  // For client-side use only!
+            let idToken = user.authentication.idToken // Safe to send to the server
+            let fullName = user.profile.name
+            let givenName = user.profile.givenName
+            let familyName = user.profile.familyName
+            let email = user.profile.email
+            
+            print("User ID: ", userId!)
+            print("ID Token: ", idToken!)
+            print("Full Name: ", fullName!)
+            print("Given Name: ", givenName!)
+            print("Family Name: ", familyName!)
+            print("Email: ", email!)
         }
 
         guard let authentication = user.authentication else { return }
@@ -58,7 +88,7 @@ extension HomeController: GIDSignInDelegate{
         let nextViewController = storyBoard.instantiateViewController(withIdentifier: "CottageTabsView") as! CottageTabsController
         nextViewController.modalPresentationStyle = .fullScreen
         
-        self.present(nextViewController, animated:true, completion:nil)
+        self.present(nextViewController, animated:false, completion:nil)
         
     }
     
