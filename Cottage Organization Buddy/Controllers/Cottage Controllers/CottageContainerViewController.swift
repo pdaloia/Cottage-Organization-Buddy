c646cedf387326d7d747bffd9fe4c5c47450825b
Philip D'Aloia <philip.daloia@gmail.com>
2021-05-19 13:11:16 -0400

    Creating the new view controllers. One for the side menu and one for the container that will hold both the cottage tabs and side menu view controllers.

---
+++ Cottage Organization Buddy/Controllers/Cottage Controllers/CottageContainerViewController.swift
@@ -0,0 +1,16 @@
+//
+//  CottageContainerViewController.swift
+//  Cottage Organization Buddy
+//
+//  Created by Philip D'Aloia on 2021-05-19.
+//
+
+import UIKit
+
+class CottageContainerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
---
+++ Cottage Organization Buddy/Controllers/Cottage Controllers/SideMenuViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SideMenuViewController.swift
+//  Cottage Organization Buddy
+//
+//  Created by Philip D'Aloia on 2021-05-19.
+//
+
+import UIKit
+
+class SideMenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
