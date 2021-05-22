8d6740c96b060233e9a80ce6c4a6fb6d0541bdd6
Philip D'Aloia <philip.daloia@gmail.com>
2021-05-22 15:00:19 -0400

    Creating the protocol we will use to sign a user out when menu item is selected

---
+++ Cottage Organization Buddy/Models/Firebase Protocols/SignOutProtocol.swift
@@ -0,0 +1,16 @@
+//
+//  SignOutProtocol.swift
+//  Cottage Organization Buddy
+//
+//  Created by Philip D'Aloia on 2021-05-22.
+//
+
+import Foundation
+
+protocol SignOutProtocol {
+    
+    func signOutCurrentUser()
+    
+    func DismissAndPopAllViewControllers()
+    
+}
