1ab11d1eb367cc085aba84e382e62158c4bb2939
Philip D'Aloia <philip.daloia@gmail.com>
2021-06-15 22:15:19 -0400

    Creating apple ID button and popup for login (login to apple/firebase not implemented yet)

--- Cottage Organization Buddy/Views/Home Page Views/HomePageView.swift
+++ Cottage Organization Buddy/Views/Home Page Views/HomePageView.swift
@@ -44,6 +44,7 @@ class HomePageView: UIView {
     override init(frame: CGRect) {
         super.init(frame: frame)
         
+        setupAppleSignInButton()
         setupView()
     }
     
@@ -95,6 +96,43 @@ class HomePageView: UIView {
         
     }
     
+    func setupAppleSignInButton() {
+        
+        appleSignInButton.addTarget(self, action: #selector(handleSignInWithAppleTapped), for: .touchUpInside)
+        
+    }
+    
+    @objc func handleSignInWithAppleTapped() {
+        
+        performAppleSignIn()
+        
+    }
+    
+    func performAppleSignIn() {
+        
+        let request = createAppleIDRequest()
+        let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+        
+        authorizationController.delegate = self
+        authorizationController.presentationContextProvider = self
+        
+        authorizationController.performRequests()
+        
+    }
+    
+    func createAppleIDRequest() -> ASAuthorizationAppleIDRequest {
+        
+        let appleIDProvider = ASAuthorizationAppleIDProvider()
+        let request = appleIDProvider.createRequest()
+        request.requestedScopes = [.fullName, .email]
+        
+        let nonce = randomNonceString()
+        request.nonce = sha256(nonce)
+        currentNonce = nonce
+        return request
+        
+    }
+    
     func startAnimatingActivitySpinner() {
         
         activitySpinner.startAnimating()
@@ -107,4 +145,68 @@ class HomePageView: UIView {
         activitySpinner.stopAnimating()
     }
     
+}
+
+extension HomePageView: ASAuthorizationControllerDelegate {
+    
+    
+    
+}
+
+extension HomePageView: ASAuthorizationControllerPresentationContextProviding{
+    
+    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+        
+        return self.window!
+        
+    }
+    
+}
+
+// Adapted from https://auth0.com/docs/api-auth/tutorials/nonce#generate-a-cryptographically-random-nonce
+private func randomNonceString(length: Int = 32) -> String {
+  precondition(length > 0)
+  let charset: Array<Character> =
+      Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
+  var result = ""
+  var remainingLength = length
+
+  while remainingLength > 0 {
+    let randoms: [UInt8] = (0 ..< 16).map { _ in
+      var random: UInt8 = 0
+      let errorCode = SecRandomCopyBytes(kSecRandomDefault, 1, &random)
+      if errorCode != errSecSuccess {
+        fatalError("Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)")
+      }
+      return random
+    }
+
+    randoms.forEach { random in
+      if remainingLength == 0 {
+        return
+      }
+
+      if random < charset.count {
+        result.append(charset[Int(random)])
+        remainingLength -= 1
+      }
+    }
+  }
+
+  return result
+}
+
+import CryptoKit
+
+// Unhashed nonce.
+fileprivate var currentNonce: String?
+@available(iOS 13, *)
+private func sha256(_ input: String) -> String {
+  let inputData = Data(input.utf8)
+  let hashedData = SHA256.hash(data: inputData)
+  let hashString = hashedData.compactMap {
+    return String(format: "%02x", $0)
+  }.joined()
+
+  return hashString
 }
